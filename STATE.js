a35872f67dd1f643b74f41e71209776594108882
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-05-21 23:35:28 +0600

    add html css javascript and react js
    It is a small project with react js ..For this project I used HTML CSS JAVASCRIPT and REACT JS

---
+++ STATE.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+
+export default class STATE extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+            count : 0
+        }
+    }
+    
+    handleIncrement = () =>{
+
+        this.setState({
+
+            count : this.state.count + 1
+        })
+    }
+    handleDecrement = () =>{
+
+        this.setState({
+
+            count : this.state.count -1
+        })
+    }
+    
+    render() {
+        const {count} = this.state;
+        return (
+            <div>
+                <h2>Count : {count}</h2>
+                <button onClick={this.handleIncrement}>+</button>
+                <button onClick={this.handleDecrement}disabled={count === 0 ? true : false}>-</button>
+            </div>
+        )
+    }
+}
